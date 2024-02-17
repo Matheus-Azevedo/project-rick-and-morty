808cf4b23ea40bc930512c89a4fb53d1639e2d9c
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-02-17 11:08:31 -0300

    Removendo arquivos desnecessários

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,10 @@
-import { Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ContainerWrapper } from './shared';
-import { useEffect, useState } from 'react';
-import { DisciplineResponse, getDisciplines } from './services';
-import { Link } from 'react-router-dom';
 
 function App() {
-  const [disciplines, setDisciplines] = useState<DisciplineResponse[]>([]);
-
-  useEffect(() => {
-    const fetchDisciplines = async () => {
-      const response = await getDisciplines();
-      setDisciplines(response);
-    };
-    fetchDisciplines();
-  }, []);
-
-  console.log(disciplines);
-
-  if (!disciplines) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <>
-      <ContainerWrapper title='Disciplinas'>
-        {disciplines.map((discipline) => (
-          <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>{discipline.name}</Card.Title>
-              <Card.Text>{discipline.description}</Card.Text>
-              <Link to={`/disciplinas/${discipline.id}`}>
-                <Button variant='primary'>Ver detalhes</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        ))}
-      </ContainerWrapper>
-    </>
+    <div className='App'>
+      <h1>Hello World</h1>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { LayoutComponent } from './components/index.ts';
-import { Disicplinas, DisciplinasDetalhe } from './pages/index.ts';
+// import App from './App.tsx';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LayoutComponent />}>
-          <Route index element={<App />} />
-          <Route path='/disciplinas' element={<Disicplinas />} />
-          <Route path='/disciplinas/:id' element={<DisciplinasDetalhe />} />
-        </Route>
-      </Routes>
+      <Routes>{/* <Route path='/' element={}></Route> */}</Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
