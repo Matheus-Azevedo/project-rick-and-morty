6084af54a7a36b10af7d373f462f258c6884e33b
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-02-20 07:02:16 -0300

    link back to home changed

--- src/components/link-back-to-home/linkBackToHome.tsx
+++ src/components/link-back-to-home/linkBackToHome.tsx
@@ -1,13 +1,11 @@
 import { ArrowLeft } from 'phosphor-react';
 import { Link } from 'react-router-dom';
-import styles from './linkBackToHome.module.css';
 
 export function LinkBackToHome() {
   return (
-    <div className={styles.container_link}>
-      <ArrowLeft size={16} />
+    <div>
       <Link to='/' className='nav-link'>
-        Back to Home
+        <ArrowLeft size={20} />
       </Link>
     </div>
   );
