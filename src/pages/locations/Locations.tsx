b33f985b06f9f6a552f5ecdc1d4ece0bace6d4ed
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-02-21 10:06:45 -0300

    modal finished

--- src/pages/locations/Locations.tsx
+++ src/pages/locations/Locations.tsx
@@ -1,6 +1,6 @@
 import { CardWrapper, NavWrapper } from '../../shared';
 import { LinkBackToHome } from '../../components';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Modal } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import { LocationResponse, getLocations } from '../../services';
 import styles from '../Pages.module.css';
@@ -8,6 +8,7 @@ import styles from '../Pages.module.css';
 export function Locations() {
   const [locations, setLocations] = useState<LocationResponse[]>([]);
   const [currentNumber, setCurrentNumber] = useState(1);
+  const [showModal, setShowModal] = useState(Array(locations.length).fill(false));
 
   useEffect(() => {
     async function fetchData() {
@@ -33,18 +34,58 @@ export function Locations() {
     }
   };
 
+  const handleShowModal = (index: number) => {
+    const newShowModal = Array(locations.length).fill(false);
+    newShowModal[index] = true;
+    setShowModal(newShowModal);
+  };
+
   return (
     <>
       <NavWrapper>
         <LinkBackToHome />
       </NavWrapper>
       <div className={styles.container_cards}>
-        {locations.map((location) => (
-          <CardWrapper>
+        {locations.map((location, index) => (
+          <CardWrapper key={location.id}>
             <Card.Title>{location.name}</Card.Title>
             <Card.Text>Type: {location.type}</Card.Text>
             <Card.Text>Dimension: {location.dimension}</Card.Text>
-            <Button variant='primary'>Go somewhere</Button>
+
+            <Button variant='primary' onClick={() => handleShowModal(index)}>
+              Show Details
+            </Button>
+
+            <Modal
+              show={showModal[index]}
+              onHide={() =>
+                setShowModal((prevState) => {
+                  const newState = [...prevState];
+                  newState[index] = false;
+                  return newState;
+                })
+              }
+              dialogClassName='modal-90w'
+              aria-labelledby='example-custom-modal-styling-title'
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id='example-custom-modal-styling-title'>
+                  <b>{location.name}</b>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Card.Text>Type: {location.type}</Card.Text>
+                <Card.Text>Dimension: {location.dimension}</Card.Text>
+                <Card.Text>
+                  <b>Characters residents:</b>
+                  {location.residents.map((character, index) => (
+                    <Card.Text key={index}>
+                      <a href={character}>{character}</a>
+                    </Card.Text>
+                  ))}
+                </Card.Text>
+              </Modal.Body>
+            </Modal>
           </CardWrapper>
         ))}
       </div>
